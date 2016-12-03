84bdc8f66c950852597d996b339cfa14cdddaf0e
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-12-02 20:47:49 -0600

    Make a minor reason for using this over a static page.

--- main.go
+++ main.go
@@ -6,16 +6,25 @@ import (
 	"os"
 )
 
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	pageData := r.URL.Path[1:]
+	w.Write([]byte(pageData))
+}
+
+func scriptHandler(w http.ResponseWriter, r *http.Request) {
+	pageData := "<script>" + r.URL.Path[1:] + "</script>"
+	w.Write([]byte(pageData))
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		pageData := "<script>" + r.URL.Path[1:] + "</script>"
-		w.Write([]byte(pageData))
-	})
+	http.HandleFunc("/file/", fileHandler)
+	http.HandleFunc("/script/", scriptHandler)
+	http.HandleFunc("/", scriptHandler) // handle scripts by default
 
 	fmt.Println("Running on port", port)
 	http.ListenAndServe(":"+port, nil)
